8d93f78bf5f4c825e616399adc32e7df48da20f3
Polat İnceler <inceler.polat@gmail.com>
2015-01-31 22:20:59 +0200

    Html 1.8.4
    - Lightbox’da siyah alana tıklandığında resmin kapanması ile ilgili bir
    düzenleme yapıldı.

--- inc/js/script.js
+++ inc/js/script.js
@@ -2,9 +2,16 @@ $(document).ready(function () {
 
     /* Lightbox Settings */
     $('a[rel="image"]').swipebox();
-    $(".video").colorbox({iframe: true, innerWidth: 800, innerHeight: 600});
-    $(".map").colorbox({iframe: true, width: "90%", height: "80%"});
-    $(".popup").colorbox({inline:true, maxWidth: "90%", maxHeight: "80%"});
+    $('.video').colorbox({iframe: true, innerWidth: 800, innerHeight: 600});
+    $('.map').colorbox({iframe: true, width: '90%', height: '80%'});
+    $('.popup').colorbox({inline:true, maxWidth: '90%', maxHeight: '80%'});
+    $(document.body)
+        .on('click touchend','#swipebox-slider .current img', function(e){
+            return false;
+        }).on('click touchend','#swipebox-slider .current', function(e){
+            $('#swipebox-close').trigger('click');
+        });
+    });
 
     /* Placeholder */
     $('input, textarea').placeholder();
@@ -13,16 +20,16 @@ $(document).ready(function () {
 
 function messageBox (message) {
     $(function () {
-        if ($("#colx").html() != null) {
-            $(".messageBoxContainer").html(message);
-            $(".inline").colorbox({inline: true, width: "auto", height: "auto"});
-            $("#cola").click();
+        if ($('#colx').html() != null) {
+            $('.messageBoxContainer').html(message);
+            $('.inline').colorbox({inline: true, width: 'auto', height: 'auto'});
+            $('#cola').click();
         } else {
-            $("#colx").remove();
-            var appendtext = '<div id="colx" style="display:none;"><a class="inline" href="#coldiv" id="cola"></a><div id="coldiv"><div style="padding:10px; font-size:18px; color:#333; text-align:center" class="messageBoxContainer">' + message + '</div></div></div>';
-            $("body").append(appendtext);
-            $(".inline").colorbox({inline: true, width: "auto", height: "auto"});
-            $("#cola").click();
+            $('#colx').remove();
+            var appendtext = '<div id="colx" style="display: none;"><a class="inline" href="#coldiv" id="cola"></a><div id="coldiv"><div style="padding:10px; font-size:18px; color:#333; text-align:center" class="messageBoxContainer">' + message + '</div></div></div>';
+            $('body').append(appendtext);
+            $('.inline').colorbox({inline: true, width: 'auto', height: 'auto'});
+            $('#cola').click();
         }
     });
 }
