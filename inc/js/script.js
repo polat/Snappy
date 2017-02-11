384f83ca4c061b44e7b673027c3c4904bc90a8a3
ozturkcangokkaya <Ozturk@mac-MacBook-Pro.local>
2017-02-11 17:22:20 +0200

    1.6.3
    - Home ikonu değiştirildi.
    - Akordiyon menüdeki scroll problemi çözüldü.

--- inc/js/script.js
+++ inc/js/script.js
@@ -2736,17 +2736,19 @@ $(document).ready(function(){
     // <---- ACCORDION  ----
     $(".s-accordion li > a").click(function(){
         var clicked = $(this);
-        var clicked_offset;
 
         $(this).toggleClass("active").parent().siblings("li").children("a").removeClass("active");
 
         $(this).siblings("div").stop().slideToggle().parents("li").siblings("li"). children("div").stop().slideUp().promise().done(function(){
-            var gutter = parseInt($(this).css("padding"));
-            clicked_offset = clicked.offset().top;
+            var gutter = parseInt($(this).css("padding-top"));
+            var clicked_offset = clicked.offset().top;
+            var scroll_target = clicked_offset - gutter;
+
+            console.log(gutter, clicked_offset);
 
             if(clicked.parents(".s-accordion").hasClass("s-accordion-autoscroll")){
                 $("html,body").animate({
-                    scrollTop: clicked_offset - gutter
+                    scrollTop: scroll_target
                 },500);
             }
         });
