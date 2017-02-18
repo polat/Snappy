94d15d7fa1d6af41bc29c711a3c0f2b672c493f2
ozturkcangokkaya <Ozturk@mac-MacBook-Pro.local>
2017-02-18 11:53:09 +0200

    1.6.4
    - “s-list-col-*" sınıfları "s-list-column-*" şeklinde değiştirildi.
    - Popup'daki yükseklik problemi çözüldü.

--- src/js/components/accordion.js
+++ src/js/components/accordion.js
@@ -7,12 +7,10 @@ $(document).ready(function(){
 
         $(this).toggleClass("active").parent().siblings("li").children("a").removeClass("active");
 
-        $(this).siblings("div").stop().slideToggle().parents("li").siblings("li"). children("div").stop().slideUp().promise().done(function(){
+        $(this).siblings("div").stop().slideToggle().parents("li").siblings("li").children("div").stop().slideUp().promise().done(function(){
             var gutter = parseInt($(this).css("padding-top"));
             var clicked_offset = clicked.offset().top;
             var scroll_target = clicked_offset - gutter;
-
-            console.log(gutter, clicked_offset);
 
             if(clicked.parents(".s-accordion").hasClass("s-accordion-autoscroll")){
                 $("html,body").animate({
