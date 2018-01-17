ec553d9f4ffb8333906207dec9dc08d0b7ec93e7
Polat İnceler <inceler.polat@gmail.com>
2018-01-17 14:52:56 +0300

    2.1
    - Yeni Toast kütüphanesi eklendi.

--- src/js/base/base.js
+++ src/js/base/base.js
@@ -19,6 +19,25 @@ $(document).ready(function () {
     });
     // ---- SCROLL TOP BUTTON ---->
 
+    // <---- TOAST OPTIONS ----
+    toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": true,
+        "progressBar": true,
+        "positionClass": "toast-bottom-left",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "2000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+    // ---- TOAST OPTIONS ---->
 
     // <---- POPUPS ----
     // Basic Popup
